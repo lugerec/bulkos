d4535242c80db525fd048e83fc159df549993769
lugerec <lu.gerec@gmail.com>
2026-07-03 10:37:31 +0200

    Add Firebase config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import "../firebase/config";
 import { useAppStore } from "../store/appStore";
 import {
   ProgressRing,
---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEm63pwqxb0PthMD9pzg9xADwMrtwLltc",
+  authDomain: "bulkos.firebaseapp.com",
+  projectId: "bulkos",
+  storageBucket: "bulkos.firebasestorage.app",
+  messagingSenderId: "808077952073",
+  appId: "1:808077952073:web:7212642bd3e091d90c6b98",
+};
+
+export const app = initializeApp(firebaseConfig);
